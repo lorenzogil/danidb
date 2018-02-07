38517e6d7af57b7b40068db199495c1156187aba
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2018-02-06 23:06:51 -0800

    feat (REPL): implement the R and L of REPL using rustyline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate rustyline;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut editor = Editor::<()>::new();
+    if let Err(_) = editor.load_history("history.txt") {
+        println!("No previous history.");
+    }
+
+    loop {
+        let readline = editor.readline(">> ");
+        match readline {
+            Ok(line) => {
+                editor.add_history_entry(&line);
+                println!("Line: {}", line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
+
+    editor.save_history("history.txt").unwrap();
 }
